689234716658bf2e2d73d54453473e3cfffad2fc
Harm Tilburgs <harm@tilburgs.com>
2026-03-08 21:46:52 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,5 @@
 const NodeHelper = require("node_helper");
-const { exec } = require("child_process");
+const ping = require("ping"); // Node dependency (no OS ping needed)
 const https = require("https");
 const http = require("http");
 const url = require("url");
@@ -13,60 +13,80 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function (notification, config) {
         if (notification === "CONFIG") {
             this.config = config;
-            this.startChecking();
+            this.checkAll(); // run immediately
+            this.schedule();
         }
     },
 
-    startChecking: function () {
+    schedule: function () {
         const self = this;
-
-        setInterval(function () {
-            self.config.systems.forEach(system => {
-                if (system.type === "ping") {
-                    self.pingHost(system.host);
-                } else if (system.type === "http") {
-                    self.httpCheck(system.host);
-                }
-            });
+        setInterval(() => {
+            self.checkAll();
         }, this.config.interval);
+    },
+
+    checkAll: function () {
+        this.config.systems.forEach(system => {
+            if (system.type === "ping") {
+                this.pingHost(system.host);
+            } else if (system.type === "http") {
+                this.httpCheck(system.host);
+            }
+        });
     },
 
     pingHost: function (host) {
         const start = Date.now();
-        exec(`ping -c 1 ${host}`, (error, stdout, stderr) => {
-            const latency = error ? null : Date.now() - start;
+        ping.promise.probe(host, {
+            timeout: 3
+        }).then(res => {
+            const latency = res.alive ? Date.now() - start : null;
             this.sendSocketNotification("STATUS_RESULT", {
                 host: host,
-                alive: !error,
+                alive: res.alive,
                 latency: latency
+            });
+        }).catch(() => {
+            this.sendSocketNotification("STATUS_RESULT", {
+                host: host,
+                alive: false,
+                latency: null
             });
         });
     },
 
     httpCheck: function (hostUrl) {
-        const start = Date.now();
-        const parsedUrl = url.parse(hostUrl);
-        const lib = parsedUrl.protocol === 'https:' ? https : http;
+        try {
+            const start = Date.now();
+            const parsedUrl = url.parse(hostUrl);
+            const lib = parsedUrl.protocol === 'https:' ? https : http;
 
-        const req = lib.get(parsedUrl, (res) => {
-            const latency = Date.now() - start;
-            const alive = res.statusCode >= 200 && res.statusCode < 400;
-            this.sendSocketNotification("STATUS_RESULT", {
-                host: hostUrl,
-                alive: alive,
-                latency: latency
+            const req = lib.get(parsedUrl, (res) => {
+                const latency = Date.now() - start;
+                const alive = res.statusCode >= 200 && res.statusCode < 400;
+                this.sendSocketNotification("STATUS_RESULT", {
+                    host: hostUrl,
+                    alive: alive,
+                    latency: latency
+                });
             });
-        });
 
-        req.on('error', () => {
+            req.on('error', () => {
+                this.sendSocketNotification("STATUS_RESULT", {
+                    host: hostUrl,
+                    alive: false,
+                    latency: null
+                });
+            });
+
+            req.setTimeout(5000, () => req.abort());
+        } catch (e) {
             this.sendSocketNotification("STATUS_RESULT", {
                 host: hostUrl,
                 alive: false,
                 latency: null
             });
-        });
-
-        req.setTimeout(5000, () => req.abort());
+        }
     }
 
 });

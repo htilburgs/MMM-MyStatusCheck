e6069f01ec7cc79c776b4592881e1035e47b4bbd
Harm Tilburgs <harm@tilburgs.com>
2026-03-14 16:37:16 +0100

    Update MMM-MyStatusCheck.js

--- MMM-MyStatusCheck.js
+++ MMM-MyStatusCheck.js
@@ -2,22 +2,10 @@ Module.register("MMM-MyStatusCheck", {
 
     defaults: {
         systems: [
-            { host: "192.168.0.1", label: "Unify Router", type: "ping",
+            { host: "192.168.0.1", label: "Router", type: "ping",
               icons: { online: "fas fa-server", offline: "fas fa-times-circle", checking: "fas fa-spinner fa-spin" },
               colors: { online: "#00ff00", offline: "#ff4444", checking: "#ffaa00" } 
             },
-            { host: "192.168.0.10", label: "Synology NAS", type: "ping",
-              icons: { online: "fas fa-server", offline: "fas fa-times-circle", checking: "fas fa-spinner fa-spin" },
-              colors: { online: "#00ff00", offline: "#ff4444", checking: "#ffaa00" } 
-            },
-            { host: "192.168.0.100", label: "DHCP Server", type: "ping",
-              icons: { online: "fas fa-database", offline: "fas fa-times-circle", checking: "fas fa-spinner fa-spin" },
-              colors: { online: "green", offline: "red", checking: "orange" } 
-            },
-            { host: "http://www.tilburgs.com", label: "TILBURGS", type: "http",
-              icons: { online: "fas fa-globe", offline: "fas fa-exclamation-circle", checking: "fas fa-spinner fa-spin" },
-              colors: { online: "green", offline: "red", checking: "orange" } 
-            }
         ],
         interval: 15000,
         showLatency: true,
